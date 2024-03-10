d3e2dc31d707be882dafeb23dc1ca062c4ecfe93
bavi2477 <bavithra.sjh@gmail.com>
2024-03-10 15:06:11 +0530

    html

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -9,11 +9,11 @@ export const renderHomePage = (req, res) => {
             <body>
                 <h1>Welcome to Bavithra's Hotel</h1>
                 <ul>
-                    <li><b>POST</b> /room-create - Create a new room</li>
-                    <li><b>POST</b> /book-room - Book a room</li>
-                    <li><b>GET</b> /all-rooms - List all rooms</li>
-                    <li><b>GET</b> /all-customers - List all customers with booked data</li>
-                    <li><b>GET</b> /booking-history/:customer_name - List booking history for a customer</li>
+                    <li><b>POST</b> /api/room-create - Create a new room</li>
+                    <li><b>POST</b> /api/book-room - Book a room</li>
+                    <li><b>GET</b> /api/all-rooms - List all rooms</li>
+                    <li><b>GET</b> /api/all-customers - List all customers with booked data</li>
+                    <li><b>GET</b> /api/booking-history/:customer_name - List booking history for a customer</li>
                 </ul>
             </body>
         </html>
