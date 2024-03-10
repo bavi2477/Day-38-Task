ff8b570c768b3ecdcf3d3760962a6bd04fedec69
bavi2477 <bavithra.sjh@gmail.com>
2024-03-10 15:02:24 +0530

    readme

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -1,16 +1,23 @@
 
 
 export const renderHomePage = (req, res) => {
-    const routes = [
-        { method: 'POST', path: '/room-create', description: 'Create a new room' },
-        { method: 'POST', path: '/book-room', description: 'Book a room' },
-        { method: 'GET', path: '/all-rooms', description: 'List all rooms' },
-        { method: 'GET', path: '/all-customers', description: 'List all customers with booked data' },
-        { method: 'GET', path: '/booking-history/:customer_name', description: 'List booking history for a customer' }
-    ];
+    let htmlResponse = `
+        <html>
+            <head>
+                <title>Welcome to Bavithra's Hotel</title>
+            </head>
+            <body>
+                <h1>Welcome to Bavithra's Hotel</h1>
+                <ul>
+                    <li><b>POST</b> /room-create - Create a new room</li>
+                    <li><b>POST</b> /book-room - Book a room</li>
+                    <li><b>GET</b> /all-rooms - List all rooms</li>
+                    <li><b>GET</b> /all-customers - List all customers with booked data</li>
+                    <li><b>GET</b> /booking-history/:customer_name - List booking history for a customer</li>
+                </ul>
+            </body>
+        </html>
+    `;
 
-    res.status(200).send({ 
-        message: "Welcome to Bavithra's hotel",
-        routes: routes
-    });
+    res.status(200).send(htmlResponse);
 };
